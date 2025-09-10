fdae0d2607aa602782e62ad6f252a48118bcd429
Florian Dreier <dreier@cqse.eu>
2025-09-10 15:49:24 +0200

    TS-44092 Replaced ListMap

--- common-system-test/src/main/kotlin/com/teamscale/test/commons/Session.kt
+++ common-system-test/src/main/kotlin/com/teamscale/test/commons/Session.kt
@@ -5,10 +5,8 @@ import com.teamscale.client.EReportFormat
 import com.teamscale.client.JsonUtils
 import com.teamscale.report.compact.TeamscaleCompactCoverageReport
 import com.teamscale.report.testwise.model.TestwiseCoverageReport
-import org.conqat.lib.commons.collections.ListMap
 import spark.Request
 import java.io.IOException
-import java.util.stream.Collectors
 
 /**
  * Represents an upload session to Teamscale, which contains data from a single partition, for a specific
@@ -25,7 +23,7 @@ class Session(request: Request) {
 	var isCommitted: Boolean = false
 		private set
 
-	private val reports = ListMap<EReportFormat, ExternalReport>()
+	private val reports = mutableMapOf<EReportFormat, MutableList<ExternalReport>>()
 
 	/**
 	 * Retrieves the commit information for the session. Combines the revision and commit data into a single string
@@ -43,54 +41,36 @@ class Session(request: Request) {
 
 	/** Adds a new report into the session.  */
 	fun addReport(format: String, report: String) {
-		reports.add(EReportFormat.valueOf(format), ExternalReport(report))
+		reports.getOrPut(EReportFormat.valueOf(format)) { mutableListOf() }.add(ExternalReport(report))
 	}
 
 	/** Returns all reports from this session.  */
-	fun getReports(): List<ExternalReport> = reports.getValues()
+	fun getReports(): List<ExternalReport> = reports.values.flatten()
 
 	/** Returns all reports of the given format.  */
-	fun getReports(format: EReportFormat): MutableList<ExternalReport> = reports.getCollection(format)
+	fun getReports(format: EReportFormat): MutableList<ExternalReport>? = reports[format]
 
 	/** Returns the only report in the given format. It asserts that there are no other reports present.  */
 	fun getOnlyReport(format: EReportFormat): String {
-		check(reports.getKeys().size == 1) {
-			"Expected exactly one report format, but got ${reports.getKeys()}!"
+		check(reports.keys.size == 1) {
+			"Expected exactly one report format, but got ${reports.keys}!"
 		}
-		check(reports.containsCollection(format)) {
+		check(reports.contains(format)) {
 			"No ${format.readableName} report found! Session contains ${
-				reports.getKeys().map(EReportFormat::readableName).toSet()
+				reports.keys.map(EReportFormat::readableName)
 			} reports."
 		}
-		check(reports.getCollection(format).size == 1) {
-			"Expected exactly one ${format.readableName} report, but got ${reports.getCollection(format).size}."
+		check(reports[format]?.size == 1) {
+			"Expected exactly one ${format.readableName} report, but got ${reports[format]?.size}."
 		}
-		return reports.getCollection(format).first().reportString
+		return reports[format]!!.first().reportString
 	}
-
-	/**
-	 * Returns the report at the given index in [reports], parsed as a [TestwiseCoverageReport].
-	 *
-	 * @throws IOException when parsing the report fails.
-	 */
-	@Throws(IOException::class)
-	fun getTestwiseCoverageReport(index: Int) =
-		reports.getCollection(EReportFormat.TESTWISE_COVERAGE).getOrNull(index)?.let {
-			JsonUtils.deserialize<TestwiseCoverageReport>(it.reportString)
-		}
 
 	@get:Throws(JsonProcessingException::class)
 	val onlyTestwiseCoverageReport: TestwiseCoverageReport
 		/** Returns the only Testwise Coverage report in deserialized form.  */
 		get() = JsonUtils.deserialize<TestwiseCoverageReport>(
 			getOnlyReport(EReportFormat.TESTWISE_COVERAGE)
-		)
-
-	@get:Throws(JsonProcessingException::class)
-	val onlyCompactCoverageReport: TeamscaleCompactCoverageReport
-		/** Returns the only Compact Coverage report in deserialized form.  */
-		get() = JsonUtils.deserialize<TeamscaleCompactCoverageReport>(
-			getOnlyReport(EReportFormat.TEAMSCALE_COMPACT_COVERAGE)
 		)
 
 	/**
@@ -100,7 +80,7 @@ class Session(request: Request) {
 	 */
 	@Throws(IOException::class)
 	fun getCompactCoverageReport(index: Int) =
-		reports.getCollection(EReportFormat.TEAMSCALE_COMPACT_COVERAGE).getOrNull(index)?.let {
+		reports[EReportFormat.TEAMSCALE_COMPACT_COVERAGE]?.getOrNull(index)?.let {
 			JsonUtils.deserialize<TeamscaleCompactCoverageReport>(it.reportString)
 		}
 }
