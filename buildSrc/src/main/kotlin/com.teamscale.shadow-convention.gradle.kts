ea74e320bed09ba43244aa47659022962912a2e0
Florian Dreier <dreier@cqse.eu>
2026-03-16 07:52:10 +0100

    Removed shadow workaround and deprecated Json methods

--- buildSrc/src/main/kotlin/com.teamscale.shadow-convention.gradle.kts
+++ buildSrc/src/main/kotlin/com.teamscale.shadow-convention.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.DependencyFilter
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
@@ -18,35 +17,4 @@ tasks.named<ShadowJar>("shadowJar") {
 	relocate("retrofit", "shadow.retrofit")
 	val archiveFile = this.archiveFile
 	doLast("revertKotlinPackageChanges") { revertKotlinPackageChanges(archiveFile) }
-}
-
-// Defer the resolution of 'runtimeClasspath'. This is an issue in the shadow
-// plugin that it automatically accesses the files in 'runtimeClasspath' while
-// Gradle is building the task graph. The lines below work around that.
-// https://github.com/GradleUp/shadow/issues/882
-tasks.withType<ShadowJar> {
-	// Do not resolve too early through 'dependencyFilter'
-	dependencyFilter = NoResolveDependencyFilter(project)
-}
-
-class NoResolveDependencyFilter(
-	project: Project
-) : DependencyFilter.AbstractDependencyFilter(project) {
-
-	// Copy of https://github.com/GradleUp/shadow/blob/main/src/main/kotlin/com/github/jengelman/gradle/plugins/shadow/internal/DefaultDependencyFilter.kt#L10
-	override fun resolve(
-		dependencies: Set<ResolvedDependency>,
-		includedDependencies: MutableSet<ResolvedDependency>,
-		excludedDependencies: MutableSet<ResolvedDependency>,
-	) {
-		dependencies.forEach {
-			if (if (it.isIncluded()) includedDependencies.add(it) else excludedDependencies.add(it)) {
-				resolve(it.children, includedDependencies, excludedDependencies)
-			}
-		}
-	}
-
-	override fun resolve(configuration: Configuration): FileCollection {
-		return configuration
-	}
 }
